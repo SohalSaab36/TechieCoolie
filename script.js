973ae13715720c89f632c5a86ae5180d01a4e674
SohalSaab36 <96253854+SohalSaab36@users.noreply.github.com>
2023-02-24 00:42:05 +0530

    Script1

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function message(info){
-    return info
-}
+
+let d = new Date();
+let currentYear = d.getFullYear();
+let birth = currentYear-2008;
+let birthh= "Hi My name is Amritpal  Singh I am a "+ birth + " and as i am learning i also want you to learn from my experiences."
+document.addEventListener('DOMContentLoaded', () =>{
+    document.querySelector('#abs').innerHTML= birthh;
+});
